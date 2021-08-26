89112937ca650a71a88063114809344df218a07b
cr0manty <denis.dudka@dataart.com>
2021-08-26 13:50:37 +0300

    fix 'flutter_jailbreak_detection/flutter_jailbreak_detection-Swift.h' file not found

--- ios/Classes/FlutterJailbreakDetectionPlugin.m
+++ ios/Classes/FlutterJailbreakDetectionPlugin.m
@@ -1,5 +1,12 @@
 #import "FlutterJailbreakDetectionPlugin.h"
-#import <flutter_jailbreak_detection/flutter_jailbreak_detection-Swift.h>
+#if __has_include(<flutter_jailbreak_detection/flutter_jailbreak_detection-Swift.h>)
+    #import <flutter_jailbreak_detection/flutter_jailbreak_detection-Swift.h>
+#else
+    // Support project import fallback if the generated compatibility header
+    // is not copied when this plugin is created as a library.
+    // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
+    #import "flutter_jailbreak_detection-Swift.h"
+#endif
 
 @implementation FlutterJailbreakDetectionPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
