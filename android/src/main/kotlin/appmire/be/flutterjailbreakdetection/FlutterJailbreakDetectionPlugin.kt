34446a2a4d4971371f01656e89515248ab246793
taint <taint@sunshinegroup.vn>
2021-11-11 11:54:42 +0700

    update ignore

--- android/src/main/kotlin/appmire/be/flutterjailbreakdetection/FlutterJailbreakDetectionPlugin.kt
+++ android/src/main/kotlin/appmire/be/flutterjailbreakdetection/FlutterJailbreakDetectionPlugin.kt
@@ -1,46 +1,69 @@
 package appmire.be.flutterjailbreakdetection
 
-import android.app.Activity
+import android.content.Context
 import android.provider.Settings
+import androidx.annotation.NonNull
 import com.scottyab.rootbeer.RootBeer
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
-class FlutterJailbreakDetectionPlugin(private val context: Activity) : MethodCallHandler {
-    companion object {
-        @JvmStatic
-        fun registerWith(registrar: Registrar): Unit {
-            if (null == registrar.activity()) {
-                return;
-            }
-            val channel = MethodChannel(registrar.messenger(), "flutter_jailbreak_detection")
-            channel.setMethodCallHandler(FlutterJailbreakDetectionPlugin(registrar.activity()))
+class FlutterJailbreakDetectionPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
+    /// The MethodChannel that will the communication between Flutter and native Android
+    ///
+    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+    /// when the Flutter Engine is detached from the Activity
+    private lateinit var channel: MethodChannel
+    private lateinit var context: Context
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_jailbreak_detection")
+        channel.setMethodCallHandler(this)
+    }
+
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        if (call.method.equals("jailbroken")) {
+            val rootBeer = RootBeer(context)
+            result.success(rootBeer.isRooted)
+        } else if (call.method.equals("developerMode")) {
+            result.success(isDevMode())
+        } else {
+            result.notImplemented()
         }
     }
 
     @android.annotation.TargetApi(17)
     fun isDevMode(): Boolean {
-        return if (Integer.valueOf(android.os.Build.VERSION.SDK) == 16) {
-            Settings.Secure.getInt(context.getContentResolver(),
-                    Settings.Secure.DEVELOPMENT_SETTINGS_ENABLED, 0) != 0
-        } else if (Integer.valueOf(android.os.Build.VERSION.SDK) >= 17) {
-            Settings.Secure.getInt(context.getContentResolver(),
+        return if (Integer.valueOf(android.os.Build.VERSION.SDK_INT) == 16) {
+            Settings.Secure.getInt(context.contentResolver,
+                    Settings.Global.DEVELOPMENT_SETTINGS_ENABLED, 0) != 0
+        } else if (Integer.valueOf(android.os.Build.VERSION.SDK_INT) >= 17) {
+            Settings.Secure.getInt(context.contentResolver,
                     Settings.Global.DEVELOPMENT_SETTINGS_ENABLED, 0) != 0
         } else
             false
     }
 
-    override fun onMethodCall(call: MethodCall, result: Result): Unit {
-        if (call.method.equals("jailbroken")) {
-            val rootBeer = RootBeer(context)
-            result.success(rootBeer.isRooted)
-        }else if (call.method.equals("developerMode")){
-            result.success(isDevMode())
-        } else {
-            result.notImplemented()
-        }
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        context = binding.activity;
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        context = binding.activity;
+    }
+
+    override fun onDetachedFromActivity() {
+
     }
 }
