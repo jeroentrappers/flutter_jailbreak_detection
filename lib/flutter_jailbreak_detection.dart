bbb0f5d3c35ced872822f2af9617180cbe233719
barrett.pyke <barrett.pyke@backstopsolutions.com>
2023-03-27 14:08:19 -0500

    bpyke1: testing

--- lib/flutter_jailbreak_detection.dart
+++ lib/flutter_jailbreak_detection.dart
@@ -6,10 +6,12 @@ class FlutterJailbreakDetection {
   static const MethodChannel _channel =
       const MethodChannel('flutter_jailbreak_detection');
 
-  static Future<bool> jailbroken({bool setLogging = true}) async {
-    bool? jailbroken = await _channel.invokeMethod<bool>('jailbroken', [
-      {"setLogging": setLogging}
-    ]);
+  static Future<bool> get jailbroken async {
+    bool? jailbroken = await _channel.invokeMethod<bool>('jailbroken');
+    // bool? jailbroken = await _channel.invokeMethod<bool>('jailbroken', [
+    //   {"setLogging": setLogging}
+    // ]);
+
     return jailbroken ?? true;
   }
 
