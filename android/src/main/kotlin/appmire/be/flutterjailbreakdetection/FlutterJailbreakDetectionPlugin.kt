6499d9ea21a0b4c8486cf8c562fcc342c695d10c
barrett.pyke <barrett.pyke@backstopsolutions.com>
2023-03-27 14:31:04 -0500

    bpyke1: fixed statement

--- android/src/main/kotlin/appmire/be/flutterjailbreakdetection/FlutterJailbreakDetectionPlugin.kt
+++ android/src/main/kotlin/appmire/be/flutterjailbreakdetection/FlutterJailbreakDetectionPlugin.kt
@@ -41,7 +41,7 @@ class FlutterJailbreakDetectionPlugin : FlutterPlugin, MethodCallHandler {
     override fun onMethodCall(call: MethodCall, result: Result): Unit {
         if (call.method.equals("jailbroken")) {
             val rootBeer = RootBeer(context)
-            val setLogging = call.argument<Boolean?>("setLogging") == true
+            val setLogging = call.argument<Boolean?>("setLogging")
             rootBeer.setLogging(setLogging)
             result.success(rootBeer.isRooted)
         } else if (call.method.equals("developerMode")) {
